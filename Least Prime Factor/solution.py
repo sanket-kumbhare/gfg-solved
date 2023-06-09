ca348d1c61d90a4053ec9e9fd1a6889846995ffa
sanket-kumbhare <sanketkumbhare1511@gmail.com>
2023-06-09 12:40:57 +0530

    gitignore added

--- Least Prime Factor/solution.py
+++ Least Prime Factor/solution.py
@@ -1,11 +1,12 @@
+# User function Template for python3
 class Solution:
-    def leastPrimeFactor (self, n):
-        # code here 
+    def leastPrimeFactor(self, n):
+        # code here
         op = [0, 1]
         for i in range(2, n + 1):
             for j in range(2, int(i ** 0.5)+1):
                 if i % j == 0:
-                    op.append(j)     
+                    op.append(j)
                     break
             else:
                 op.append(i)
--- Predecessor and Successor/solution.py
+++ Predecessor and Successor/solution.py
@@ -1,3 +1,14 @@
+'''
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+'''
+
+
+# This function finds predecessor and successor of key in BST.
+# It sets pre and suc as predecessor and successor respectively
 class Solution:
     def findPreSuc(self, root, pre, suc, key):
         # Your code goes here
